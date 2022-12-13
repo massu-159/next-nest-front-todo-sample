b3561b4e55e4b3550f784a7342645dce5333217e
massu-159 <magjtwjp@gmail.com>
2022-12-13 21:24:59 +0900

    hooks: create useQuery

---
+++ hooks/useQueryTasks.ts
@@ -0,0 +1,23 @@
+import { Task } from '@prisma/client'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+export const useQueryTasks = () => {
+  const router = useRouter()
+  const getTasks = async () => {
+    const { data } = await axios.get<Task[]>(`${process.env.NEXT_PUBLIC_API_URL}/todo`)
+    return data
+  }
+
+  return useQuery<Task[], Error>({
+    queryKey: ['tasks'],
+    queryFn: getTasks,
+    onError: (err: any) => {
+      if (err.response.status === 401 || err.response.atatus === 403) {
+        router.push('/')
+      }
+    },
+  })
+}
