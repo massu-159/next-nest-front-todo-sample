3e047dfd29eb00deab9081e6b85d542bf6237254
massu-159 <magjtwjp@gmail.com>
2022-12-13 16:52:42 +0900

    CPC: create userInfo

---
+++ components/UserInfo.tsx
@@ -0,0 +1,12 @@
+import { Loader } from '@mantine/core'
+import React from 'react'
+import { useQueryUser } from '../hooks/useQueryUser'
+
+export const UserInfo = () => {
+  const { data: user, status } = useQueryUser()
+  if (status === 'loading') return <Loader></Loader> 
+
+  return (
+    <p>{ user?.email }</p>
+  )
+}
